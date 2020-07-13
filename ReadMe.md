d94a8d386833bce1cb8df4838680639b5a90a702
AlexJ2005 <alexanderjernstrom@gmail.com>
2020-07-13 22:11:27 +0200

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# How to start
+
+1. Run npm install
+
+2. Create a typeDefs and resolvers file and import it in index.js
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+
+const startServer = async () => {
+  const app = express();
+
+
+  //TODO: Create resolver and typeDefs files
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app });
+
+
+  app.listen({ port: 5001 }, () =>
+    console.log(`🚀 Server ready at http://localhost:5001${server.graphqlPath}`)
+  );
+};
+
+startServer();
